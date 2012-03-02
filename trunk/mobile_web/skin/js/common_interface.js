6a3834531c22b4239cce8af711638a7d3c0aa504
master@newstools.kr <master@newstools.kr@44510c72-d478-870d-8d0c-1beebeabfd58>
2012-03-02 18:08:02 +0000

    디오티스 모바일 웹 : 사고접수2 완료버전

--- trunk/mobile_web/skin/js/common_interface.js
+++ trunk/mobile_web/skin/js/common_interface.js
@@ -8,21 +8,52 @@
 /*#endregion											*/
 
 /* STR : 기본 인터페이스용 함수*/
+	//데이터 분리구분자 및 해당 처리 함수
+	var _splitkey_ = "#";	//'#'은 쉽게 입력이 가능하므로, '\t'를 제안드립니다.
+	function SplitData(arg) {	return arg.split(_splitkey_);	}
+	function MergeData(list, data) {	return (list + _splitkey_ + data);	}
+
+
+	//데이터 검증 반환용 인터페이스 선언 : 페이지마다 별도 구현 필요
 	function readWebData() {
 		return '';
 	}
 
+	//Phone으로 부터 초기값을 전달받는 인터페이스 선언 : 페이지마다 별도 구현 필요
+	function WmWriteMsg(arg) {
+		var arryData = new Array();
+		arryData = SplitData(arg);
+	}
+
+
+	//Phone에서 데이터를 읽어들이는 함수 : 입력에러 발생시에는 데이터 전달 중지
 	function WmReadMsg(arg) {
-		if (arg=="android") {
-			window.HybridApp.javascriptToAppMessage( readWebData() );	// android 메소드 호출
+		var data = readWebData();
+		if ( data=='' ) {	//입력에러 발생시에는 빈 문자열
+			return;
+		}
+		else if (arg=="android") {
+			window.HybridApp.javascriptToAppMessage(  );	// android 메소드 호출
 		}
 		else {
 			windows.location="jsall://"+readWebData();
 		}
 	}
-
-	function WmWriteMsg(arg) {
-		var arryData = new Array();
-		arryData = arg.split("#");
-	}
 /* END : 기본 인터페이스용 함수*/
+
+
+/* STR : jQuery 메소드 선언 */
+(function ($) {
+	//이미지 태그 소스 파일명 교체
+	$.fn.ImageStateChanger = function (stateVal, onstate, offstate) {
+		if( stateVal )
+		{
+			$(this).attr('src', $(this).attr('src').replace(offstate, onstate));
+		}
+		else
+		{
+			$(this).attr('src', $(this).attr('src').replace(onstate, offstate));
+		}
+	};
+})(jQuery);
+/* END : jQuery 메소드 선언 */
